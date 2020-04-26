dc4c97e77f891de54ef72d0ce122dd6b9e9c577d
Jacob Mitash <jacobmitash@gmail.com>
2020-04-25 22:24:35 -0500

    Cause compilation error on unsupported OSs

--- client/monitor/device/src/MonitorDiscovererFactory.cpp
+++ client/monitor/device/src/MonitorDiscovererFactory.cpp
@@ -8,6 +8,7 @@
 std::unique_ptr<MonitorDiscoverer> MonitorDiscovererFactory::getMonitorDiscoverer() const {
 #ifdef _WIN32
     return std::unique_ptr<MonitorDiscoverer>{new WindowsMonitorDiscoverer{}};
+#else
+#error "Unsupported Operating System. Currently, only Windows is supported."
 #endif
-    return std::unique_ptr<MonitorDiscoverer>{nullptr};
 }
