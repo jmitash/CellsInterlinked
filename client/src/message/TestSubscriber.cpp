a3427a4ec367fbe3673763f48113e22e7a0d2d64
Jacob Mitash <jacobmitash@gmail.com>
2020-05-04 01:38:48 -0500

    Delete test classes in sources

--- client/src/message/TestSubscriber.cpp
+++
@@ -1,7 +0,0 @@
-
-#include "message/TestSubscriber.h"
-
-
-std::shared_ptr<Event> TestSubscriber::castEvent(const std::shared_ptr<Event> &event) {
-    return std::dynamic_pointer_cast<TestEvent>(event);
-}
