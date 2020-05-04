5c3bd446b069317b6918413500651bbe1228ca34
Jacob Mitash <jacobmitash@gmail.com>
2020-05-03 23:53:18 -0500

    Throw exception when no supported subscriber for event

--- client/include/message/Broker.h
+++ client/include/message/Broker.h
@@ -27,6 +27,7 @@ public:
     /**
      * Publish an event. The correct queue for the event will be determined, and then the event will be added to that queue.
      * @param event the event to add to the queue
+     * @throws std::runtime_error when no enrolled queue supports the given event
      */
     void publish(const std::shared_ptr<Event> &event);
 
--- client/src/message/Broker.cpp
+++ client/src/message/Broker.cpp
@@ -18,5 +18,11 @@ void Broker::publish(const std::shared_ptr<Event> &event) {
     };
 
     auto subscriberQueue = std::find_if(mSubscriberQueues.begin(), mSubscriberQueues.end(), supportsEvent);
+
+    if (subscriberQueue->get() == nullptr) {
+        throw std::runtime_error("Could not find a supporting subscriber queue for event. Ensure that the required "
+                                 "subscriber is enrolled before publishing.");
+    }
+
     subscriberQueue->get()->addEvent(event);
 }
